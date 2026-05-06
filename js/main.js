25783a0c91fe7976948838b9573d910292975e8e
rradwakhaledd <rradwakhaledd2002@gmail.com>
2026-05-06 15:44:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -195,7 +195,7 @@ function addAppleCalendar() {
  *  Location for Google and Waze
   ======================================================= */
 function openGoogleMaps() {
-    window.open("https://share.google/eDjOG0c1RMMVqKBEu", "_blank");
+    window.open("https://maps.app.goo.gl/h32pbzqr5WPa54fVA?g_st=aw", "_blank");
 }
 // function openWaze() {
 //     const latitude = 3.1575;  // Example latitude
